b8a3f392cfeffabe33f570ea6f841d88f75d8205
Vanilla <osu_Vanilla@126.com>
2021-10-21 20:57:39 +0800

    [+] flake.nix: Add MIST-Raspi deploy.nodes.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware, deploy-rs, ... }: rec {
     nixosConfigurations."MIST-Raspi" = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
@@ -12,6 +13,17 @@
         nixos-hardware.nixosModules.raspberry-pi-4
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
       ];
+    };
+
+    deploy.nodes."MIST-Raspi" = rec {
+      hostname = "192.168.110.24";
+      sshUser = "root";
+
+      profiles.system = {
+        user = "${sshUser}";
+        path = deploy-rs.lib."aarch64-linux".activate.nixos nixosConfigurations."MIST-Raspi";
+        fastConnection = true;
+      };
     };
   };
 }
