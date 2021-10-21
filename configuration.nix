23e8d77c6b84da991cc65461187e8521a20bbacd
Vanilla <osu_Vanilla@126.com>
2021-10-21 17:45:48 +0800

    [+] users.users."akisamu".authorizedKeys.keys.

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 {
   sdImage.compressImage = false;
-  imports = [ ./networking/wireless.nix ./users.nix ];
+  imports = [
+    ./networking/wireless.nix
+    ./users.nix
+    ./services/openssh.nix
+  ];
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
   };
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  services.openssh.enable = true;
 }
