379c1d3b23ba04f2330f50a9fe9461effc0089d9
Vanilla <osu_Vanilla@126.com>
2021-10-21 22:04:15 +0800

    [+] networking/wireless.nix: Additional WiFi hostpot.

--- networking/wireless.nix
+++ networking/wireless.nix
@@ -3,6 +3,7 @@
   networking.wireless.enable = true;
   networking.wireless.networks = {
     "@Ruijie-s02C6".psk = "Jxustnc001";
+    "5522少年天团".psk = "asdfghjkl";
   };
   networking.wireless.interfaces = [ "wlan0" ];
   networking.interfaces."wlan0".useDHCP = true;
